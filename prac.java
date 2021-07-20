9073adafcfdfad733790ed2a5767d2e704c3e420
0AaalbatrosS GuY <thexcelsiorisback@gmail.com>
2021-07-20 08:04:16 +0530

    Some school stuff

--- prac.java
+++ prac.java
@@ -3,10 +3,20 @@ import java.io.*;
 class prac{
     public static void main(String args[]) throws IOException{
         //System.out.println("Helo bruh world");
-        InputStreamReader in = new InputStreamReader(System.in);
-        BufferedStreamReader out = new BufferedStreamReader(in);
-        int num1 = br.parseInt();
-        System.out.println(ch3);
-
+        //int x = 5;
+        //float result = (x+3)/6;
+        //float result2 = (2*x+5)/3;
+        //float answer = result - result2;
+        //System.out.println(answer);
+        //int PRESENT = 350;
+        //int ABSENT = 30;
+        //int monthly_income = PRESENT*25;
+        //int absent = ABSENT*5;
+        //int monthlyincome = monthly_income - absent;
+        //System.out.println(monthlyincome);
+        int QUESTIONS = 150;
+        int candidate1 = 80/100*QUESTIONS;
+        int candidate2 = 72/100*QUESTIONS;
+        System.out.println(candidate1+" "+candidate2);
     }
     }
