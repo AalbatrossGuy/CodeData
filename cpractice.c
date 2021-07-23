24bbb12691641e08d966775190c145307d2f2115
0AaalbatrosS GuY <thexcelsiorisback@gmail.com>
2021-07-23 17:21:01 +0530

    Renamed justprac.c to cpractice.c

---
+++ cpractice.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+//int AverageMarks(int suba, int subb, int subc);
+
+int main(void){
+//    int result = AverageMarks(20, 30, 40);
+//    printf("%d", result);
+//
+}
+
+//int AverageMarks(int suba, int subb, int subc){
+//    int average = (suba+subb+subc)/3;
+//   return average;
+//}
