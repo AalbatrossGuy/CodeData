05a20cd5cc1979177ed33e44241c4d9cf18e28f4
0AaalbatrosS GuY <thexcelsiorisback@gmail.com>
2021-08-23 08:03:49 +0530

    some more stuff

--- prac.py
+++ prac.py
@@ -1,5 +1,30 @@
 import robloxpy
 
 userID = robloxpy.User.External.GetID("AalbatrossGuy")
-url = robloxpy.User.Friends.External.GetAll(userID)
-print(url)
+url = robloxpy.User.External.GetDescription(userID)
+print(userID)
+
+date = robloxpy.User.External.CreationDate(userID, 1)
+print(date)
+
+print(len(url))
+stuff = robloxpy.User.External.GetLimiteds(userID)[0][:10]
+stuff.append("...truncated")
+truncatedone = "\n".join(stuff)
+
+if len(robloxpy.User.External.GetLimiteds(userID)[0]) !=0 and len(robloxpy.User.External.GetLimiteds(userID)[0])< 11:
+    print(len(robloxpy.User.External.GetLimiteds(userID)[0]))
+    print("ran exception")
+    stuff = "\n".join(robloxpy.User.External.GetLimiteds(userID)[0])
+elif len(robloxpy.User.External.GetLimiteds(userID)[0]) > 11:
+    stuff = truncatedone
+else:
+    stuff = None
+
+
+age = robloxpy.User.Friends.External.GetCount(userID)
+name = robloxpy.User.Friends.External.GetAll(userID)
+print(age, name)
+
+followers = robloxpy.User.Friends.External.GetFollowerCount(userID)
+print(followers)
