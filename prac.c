99d9c624b26b28e423439a179e36a644cb4eebbd
0AaalbatrosS GuY <thexcelsiorisback@gmail.com>
2021-08-24 18:36:34 +0530

    Tried some more calloc stuff

--- prac.c
+++ prac.c
@@ -2,9 +2,19 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-
-
+int test(void);
 
 int main(void){
+    /*printf("Hello, World");*/
+    /*printf("\n");*/
+    test();
     return 0;
+}
+
+int test(void){
+    char *mstring = NULL;
+    mstring = (char *) calloc(11, sizeof(char)); 
+    fgets(mstring, 10, stdin);
+    printf("%s\n", mstring);
+    free(mstring);
 }
