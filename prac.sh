c165884bfbbe0b61b2995eace539c5aa39456a90
0AaalbatrosS GuY <thexcelsiorisback@gmail.com>
2021-08-24 18:37:05 +0530

    More bash scripting

--- prac.sh
+++ prac.sh
@@ -1,26 +1,33 @@
-! /usr/bin/bash
+#! /usr/bin/bash
+#read -p "Enter your name please: " IN_NAME
+#if [ "$IN_NAME" == "foo" ] ;then
+    #echo "Hi $IN_NAME"
+#elif [ "$IN_NAME" == "bar" ] 
+#then
+    #echo "$IN_NAME, hi bar!"
+#else
+    #echo "$IN_NAME, you are not XD"
+#fi
 
-read -p "Enter your name please: " IN_NAME
-if [ "$IN_NAME" == "foo" ] ;then
-    echo "Hi $IN_NAME"
-elif [ "$IN_NAME" == "bar" ] 
-then
-    echo "$IN_NAME, hi bar!"
-else
-    echo "$IN_NAME, you are not XD"
-fi
+#read -p "Love or Hate? Y/N: " ANS
+#case "$ANS" in 
+    #[yY] | [yY][eE][sS]) # case 1
+        #echo "Love it is!" #do this
+        #;;
+    #[nN] | [nN][oO]) # case 2
+        #echo "Hate it is!" # do that
+        #;; # break statement
+    #*) # end of cases
+        #echo "Please enter either yes/no (y/n)" # default (if no cases are true)
+        #;;
 
-read -p "Love or Hate? Y/N: " ANS
-case "$ANS" in 
-    [yY] | [yY][eE][sS]) # case 1
-        echo "Love it is!" #do this
-        ;;
-    [nN] | [nN][oO]) # case 2
-        echo "Hate it is!" # do that
-        ;; # break statement
-    *) # end of cases
-        echo "Please enter either yes/no (y/n)" # default (if no cases are true)
-        ;;
-esac # end case
+#esac # end case
 
-echo "done"
+#TNAME=$(ls) # need brackets to iterate over a command.
+#for NAME in $TNAME; do
+    #echo "Hello, $NAME"
+#done
+
+#echo "done"
+
+
