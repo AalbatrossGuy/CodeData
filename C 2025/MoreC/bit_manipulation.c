e35b73631fbaef279f5457d659e029b5ac42cd59
AalbatrossGuy <68378354+AalbatrossGuy@users.noreply.github.com>
2025-05-31 20:21:05 +0530

    Update for bit manipulation

---
+++ C 2025/MoreC/bit_manipulation.c
@@ -0,0 +1,22 @@
+//
+// Created by AG on 5/31/2025.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// A byte consists of eight smaller units called bits.
+// 1 byte = 8 bits.
+// Each 1 and 0 in a binary number system represent 1 bit. 1 = bit "turned on". 0 = bit "turned off".
+// The rightmost bit of a byte is known as the least significant or low-order bit, and the leftmost bit is known as the most significant or high-order bit.
+// Bytes are often referred to as words.
+// Changing a bit to 1 is known as "setting the bit"
+// Changing the bit to 0 is known as "resetting the bit"
+// unsigned = +ve
+// signed = -ve/+ve
+// signed (negative) numbers are represented using two's complement notation.
+// The leftmost bit represents the sign bit. 1 = -ve number and 0 = +ve number. The remaining bits represent the value of the number
+// Bit manipulation in C allows the manipulation of individual bits in a variable.
+int main() {
+    return 0;
+}
