135f4a8835541410fc03d4bf922aeb2756cdd7eb
AalbatrossGuy <thexcelsiorisback@gmail.com>
2025-07-21 16:56:40 +0530

    Update notes

---
+++ C 2025/more_pointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void double_pointers();
+
+int main() {
+    double_pointers();
+    return 0;
+}
+
+void double_pointers() {
+   // When a pointer holds the address of another pointer, that's known as a pointer-to-pointer or double pointer.
+}
