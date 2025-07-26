7e8e3ef4f7ad3fa3f3ce5395a4651a2c13568c76
AalbatrossGuy <thexcelsiorisback@gmail.com>
2025-07-26 11:29:00 +0530

    Add current notes

--- Rust/src/refresher_2.rs
+++ Rust/src/refresher_2.rs
@@ -43,9 +43,23 @@ fn main() {
     // bank inside "steal_bank", the bank object is now owned by steal_bank and bank has no
     // ownership of it anymore. Essentially, the bank binding is now empty and hence printing bank
     // will return an error as it contains nothing.
+    // The same happens for print_acc(myaccount). The first print statement moves the myaccount
+    // value into it so when the second print statement fetches myaccount's value, it's empty since
+    // the value is currently stored in the first print statement.
     let bank = Bank::new();
-    let steal_bank = bank;
+
+    // similar to C. & gets a reference to the value of bank. A system of "borrowing"
+    // it is basically a read-only reference that creates a "copy" of the original variable, i.e.,
+    // bank in this case. & operator used on a parameter means "The argument needs to be a
+    // reference to a value", i.e., it only takes values which are reference to another value.
+    // & operator used on a value means "create a reference to the value its used on".
+    let bank_reference = &bank;
+    // let steal_bank = bank;
     // let myaccount = Account::new(253, String::from("Mooda"));
-    // println!("{:#?}", bank);
+    println!("{:#?}", bank_reference);
+    println!("{:#?}", &bank); // Directly passing value as a reference to a value.
+    println!("{:#?}", bank);
+    // println!("{:#?}", bank.accounts);
+    // print_acc(myaccount);
     // print_acc(myaccount);
 }
